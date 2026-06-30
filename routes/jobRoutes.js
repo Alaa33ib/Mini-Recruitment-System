5363ad2f6aab650f953d6e39b2d1b1e5438d5f95
Alaa33ib <alaa33ib@gmail.com>
2026-06-30 16:26:04 +0300

    feat: implement individual job details caching and automatic cache invalidation

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import Job from "../models/Job.js";
 import { verifyToken, authorizeRoles } from "../middleware/auth.js";
 import { cacheResponse } from "../middleware/caching.js";
+import { redisClient } from "../config/redis.js";
 
 const router = express.Router();
 
@@ -15,6 +16,9 @@ router.post('/', verifyToken, authorizeRoles('recruiter'), async (req, res, next
         });
 
         await newJob.save();
+
+        await redisClient.del("cache:/api/jobs");
+        console.log("Global jobs cache deleted due to new job post")
 
         res.status(201).json({
             success: true,
@@ -35,6 +39,25 @@ router.get('/', verifyToken, cacheResponse(300), async (req, res, next)=>{
             success: true,
             count: jobs.length,
             data: jobs
+        });
+    } catch(error){
+        next(error);
+    }
+});
+
+router.get('/:id', verifyToken, cacheResponse(300), async (req, res, next)=>{
+    try{
+        const job = await Job.findById(req.params.id).populate("postedBy", "username company");
+
+        if(!job){
+            const error = new Error("Job not found");
+            error.statusCode = 404;
+            return next(error);
+        }
+
+        res.status(200).json({
+            success: true,
+            data: job
         });
     } catch(error){
         next(error);
